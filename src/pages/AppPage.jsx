56711672f4aef42047601cc1c0ec2aa787b69963
Tomer Revah <revahtomer@gmail.com>
2023-01-04 23:59:36 +0200

    fixed btn

--- src/pages/AppPage.jsx
+++ src/pages/AppPage.jsx
@@ -1,14 +1,17 @@
 import homeSvg from '../assets/img/animated_tasks.gif';
 import { Link } from 'react-router-dom';
-export function AppPage() {
+export function AppPage({ history }) {
   return (
     <div className='app-page'>
       <section className='home' id='home'>
         <div className='content'>
           <h3 className='right'>Welcome</h3>
           <p>Welcome to MyTimelineEvents App</p>
-          <button className='home-page-btn clean-btn '>
-            <Link to='timeline'> To My Timeline</Link>
+          <button
+            className='home-page-btn clean-btn'
+            onClick={() => history.push('/timeline')}
+          >
+            To My Timeline
           </button>
         </div>
         <div className='image'>
